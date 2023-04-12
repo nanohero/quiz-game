321708ac2feac9ff5bba1ec45871cbcd8d5bc688
corey <corey@LAPTOP-6TT89VSV>
2023-04-11 21:38:48 -0700

    Add project files.

---
+++ quiz game/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numRight = 0;
+int numWrong = 0;
+String name = "";
+string answerInput;
+
+Console.WriteLine("quiz questions taken from collegiate.quizbowlpackets.com");
+Console.WriteLine("Welcome to the quiz game. Please enter your name");
+Console.Write("Name:");
+name= Console.ReadLine();
+Console.WriteLine("Welcome " + name +"!");
+Console.WriteLine("\n\n\nPrese any key to continue...");
+Console.ReadLine();
+Console.WriteLine("Question #1: Calculate the Geometric Mean of 4, 5 and 6.25");
+Console.Write("Answer:");
+answerInput = Console.ReadLine();
+if (answerInput=="5")
+{
+    Console.WriteLine("CORRECT");
+}
+else
+{
+    Console.WriteLine("WRONG!");
+}
+Console.WriteLine(answerInput);
